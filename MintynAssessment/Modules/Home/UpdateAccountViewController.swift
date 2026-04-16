96f445fac24de697e9d36b7c0003f0a1e92b8cd6
Paul <udegbuepaul43@gmail.com>
2026-04-16 11:32:16 +0100

    building phase 5 refactoring all pages 8

---
+++ MintynAssessment/Modules/Home/UpdateAccountViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class UpdateAccountViewController: FeatureBaseViewController {
+    init() { super.init(title: "Update Account", icon: "info.circle.fill") }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let desc = UILabel()
+        desc.text = "Upgrade your Tier limits."
+        desc.textColor = .lightGray
+        desc.textAlignment = .center
+        desc.font = .systemFont(ofSize: 16)
+        desc.translatesAutoresizingMaskIntoConstraints = false
+        mainContainer.addSubview(desc)
+        NSLayoutConstraint.activate([
+            desc.centerXAnchor.constraint(equalTo: mainContainer.centerXAnchor),
+            desc.topAnchor.constraint(equalTo: mainContainer.topAnchor, constant: 100)
+        ])
+    }
+}
